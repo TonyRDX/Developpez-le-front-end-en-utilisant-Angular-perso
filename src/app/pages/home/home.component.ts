ae6c92ff00ba3cfe2dc16914e06102b7ef8722c2
TonyCDalvin <83288223+TonyCDalvin@users.noreply.github.com>
2025-08-19 16:17:31 +0200

    feat: home page with rxjs and pie chart

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { LegendPosition } from '@swimlane/ngx-charts';
+
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import type { Olympic } from 'src/app/core/models/Olympic';
 import type { Participation } from 'src/app/core/models/Participation';
@@ -12,11 +14,36 @@ type ChartData = { name: string; value: number, extra: number };
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
   public olympics$: Observable<Olympic[] | null> = of(null); // null could be used to notify error
   public single$: Observable<ChartData[]> = of([]);
 
   constructor(private olympicService: OlympicService) {}
+
+  @ViewChild('chartContainer', { static: true }) chartContainer!: ElementRef<HTMLDivElement>;
+  view: [number, number] = [0, 0];
+  private ro?: ResizeObserver;
+
+  ngAfterViewInit() {
+    Promise.resolve().then(() => {
+      this.updateViewSize();
+    });
+
+    this.ro = new ResizeObserver(() => {
+      this.updateViewSize();
+    });
+
+    this.ro.observe(this.chartContainer.nativeElement);
+  }
+
+  ngOnDestroy() {
+    this.ro?.disconnect();
+  }
+
+  private updateViewSize() {
+    const el = this.chartContainer.nativeElement;
+    this.view = [Math.max(0, el.clientWidth - 20), Math.max(0, el.clientHeight - 20)];
+  }
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
@@ -25,31 +52,41 @@ export class HomeComponent implements OnInit {
     );
   }
 
-  view: [number, number] = [700, 400];
-
   // options
-  showXAxis = true;
-  showYAxis = true;
-  gradient = false;
-  showLegend = true;
-  showXAxisLabel = true;
-  xAxisLabel = 'Country';
-  showYAxisLabel = true;
-  yAxisLabel = 'Medals';
+  gradient: boolean = false;
+  showLegend: boolean = false; // renders really bad because it is added outised
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
+  legendPosition: LegendPosition = LegendPosition.Below;
+  maxLabelLength: number = 12;
 
   colorScheme = 'cool'; // check values there : https://swimlane.github.io/ngx-charts/#/ngx-charts/bar-vertical
 
   toChartData(olympic: Olympic): ChartData {
     return {
       "name": olympic.country,
-      "value": olympic.participations.reduce((sum: number, participation: Participation) => sum + participation.medalsCount, 0),
+      "value": olympic.participations.reduce(
+        (sum: number, participation: Participation) => sum + participation.medalsCount, 0
+      ),
       "extra": olympic.id
     };
   }
 
+  tooltipText({ data }: any): string {
+    return `${data.name}<br />🏅${data.value}`;
+  }
+
   // TODO use that event hander to route on the selected country
-  onSelect(event: ChartData) {
-    console.log(event); 
-    console.log(JSON.stringify(event)); 
+  onSelect(data: any): void {
+    var countryName: string = data?.name || data;
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data: any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data: any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
 }
