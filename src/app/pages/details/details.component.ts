8a2ed4e0c9de1486f822387dd65dba0ed640553c
TonyCDalvin <83288223+TonyCDalvin@users.noreply.github.com>
2025-08-19 17:12:46 +0200

    feat: line chart (hello world)

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -16,7 +16,134 @@ type ChartData = { name: string; value: number, extra: number };
 })
 export class DetailsComponent implements OnInit, AfterViewInit, OnDestroy {
   public olympics$: Observable<Olympic[] | null> = of(null); // null could be used to notify error
-  public single$: Observable<ChartData[]> = of([]);
+  public multi$: Observable<ChartData[]> = of([]);
+  public multi = [
+    {
+      "name": "United Kingdom",
+      "series": [
+        {
+          "value": 2062,
+          "name": "2016-09-22T09:39:26.191Z"
+        },
+        {
+          "value": 2863,
+          "name": "2016-09-13T23:28:23.840Z"
+        },
+        {
+          "value": 6920,
+          "name": "2016-09-13T15:17:32.906Z"
+        },
+        {
+          "value": 4834,
+          "name": "2016-09-23T10:35:54.956Z"
+        },
+        {
+          "value": 6252,
+          "name": "2016-09-14T14:49:40.786Z"
+        }
+      ]
+    },
+    {
+      "name": "South Sudan",
+      "series": [
+        {
+          "value": 6384,
+          "name": "2016-09-22T09:39:26.191Z"
+        },
+        {
+          "value": 2564,
+          "name": "2016-09-13T23:28:23.840Z"
+        },
+        {
+          "value": 5674,
+          "name": "2016-09-13T15:17:32.906Z"
+        },
+        {
+          "value": 6524,
+          "name": "2016-09-23T10:35:54.956Z"
+        },
+        {
+          "value": 3189,
+          "name": "2016-09-14T14:49:40.786Z"
+        }
+      ]
+    },
+    {
+      "name": "Mongolia",
+      "series": [
+        {
+          "value": 3047,
+          "name": "2016-09-22T09:39:26.191Z"
+        },
+        {
+          "value": 3060,
+          "name": "2016-09-13T23:28:23.840Z"
+        },
+        {
+          "value": 4242,
+          "name": "2016-09-13T15:17:32.906Z"
+        },
+        {
+          "value": 3993,
+          "name": "2016-09-23T10:35:54.956Z"
+        },
+        {
+          "value": 2320,
+          "name": "2016-09-14T14:49:40.786Z"
+        }
+      ]
+    },
+    {
+      "name": "Mongolia",
+      "series": [
+        {
+          "value": 6691,
+          "name": "2016-09-22T09:39:26.191Z"
+        },
+        {
+          "value": 4581,
+          "name": "2016-09-13T23:28:23.840Z"
+        },
+        {
+          "value": 5210,
+          "name": "2016-09-13T15:17:32.906Z"
+        },
+        {
+          "value": 2421,
+          "name": "2016-09-23T10:35:54.956Z"
+        },
+        {
+          "value": 3137,
+          "name": "2016-09-14T14:49:40.786Z"
+        }
+      ]
+    },
+    {
+      "name": "Christmas Island",
+      "series": [
+        {
+          "value": 3176,
+          "name": "2016-09-22T09:39:26.191Z"
+        },
+        {
+          "value": 3626,
+          "name": "2016-09-13T23:28:23.840Z"
+        },
+        {
+          "value": 3022,
+          "name": "2016-09-13T15:17:32.906Z"
+        },
+        {
+          "value": 2836,
+          "name": "2016-09-23T10:35:54.956Z"
+        },
+        {
+          "value": 3180,
+          "name": "2016-09-14T14:49:40.786Z"
+        }
+      ]
+    }
+  ];
 
   constructor(private olympicService: OlympicService) {}
 
@@ -47,18 +174,22 @@ export class DetailsComponent implements OnInit, AfterViewInit, OnDestroy {
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
-    this.single$ = this.olympics$.pipe(
-      map((list: Olympic[] | null = []) => list?.map((o: Olympic) => this.toChartData(o)) || [])
-    );
+    // this.single$ = this.olympics$.pipe(
+    //   map((list: Olympic[] | null = []) => list?.map((o: Olympic) => this.toChartData(o)) || [])
+    // );
   }
 
   // options
-  gradient: boolean = false;
-  showLegend: boolean = false; // renders really bad because it is added outised
+  legend: boolean = true;
   showLabels: boolean = true;
-  isDoughnut: boolean = false;
-  legendPosition: LegendPosition = LegendPosition.Below;
-  maxLabelLength: number = 12;
+  animations: boolean = true;
+  xAxis: boolean = true;
+  yAxis: boolean = true;
+  showYAxisLabel: boolean = true;
+  showXAxisLabel: boolean = true;
+  xAxisLabel: string = 'Year';
+  yAxisLabel: string = 'Population';
+  timeline: boolean = true;
 
   colorScheme = 'cool'; // check values there : https://swimlane.github.io/ngx-charts/#/ngx-charts/bar-vertical
 
